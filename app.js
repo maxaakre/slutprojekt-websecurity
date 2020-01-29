ca88b78cb6962eb73c0f9dde5f973ecdef420b76
David Lundholm <david.lundholm@zocom.se>
2020-01-29 10:29:11 +0100

    Added documentation for authorization

--- app.js
+++ app.js
@@ -4,5 +4,20 @@ const app = express()
 app.use(express.static('public'))
 
 
+app.get('/api/products', (req,res) => {
+    res.json(
+        [
+            {
+                _id: 1,
+                title: "Awesome shit",
+                price: 1337,
+                shortDesc: "lorem ipsum",
+                longDesc: "loredm dfsfsafmlfds",
+                imgFile: 'skateboard-greta.png'
+            }
+        ]
+    )
+})
+
 
 app.listen(8080, () => console.log("Server started"))
