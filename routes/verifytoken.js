09a6c7d2e8eb505929db50e114bbb5870bfe5b66
maxaakre <maxaakre@gmail.com>
2020-02-10 17:01:29 +0100

    added response in verify catch

--- routes/verifytoken.js
+++ routes/verifytoken.js
@@ -8,12 +8,12 @@ module.exports =  {
      const secret = process.env.SECRET   
      const token = req.headers.authorization
      if(!token){
-        res.status(403).send({message: "no token supplied"})
+        return false
      }try{
          const verify  = await jwt.verify(token.replace("Bearer ", "" ),secret)
          req.user = verify
      }catch(err){
-         console.log(err)
+        res.status(403).send({message: "no token supplied"})
      }
      next()
     }
